4ade162d4fc7443964b183082cc5e1c949a715aa
AchillesBoketo <50740610+AchillesBoketo@users.noreply.github.com>
2022-12-05 11:01:29 +0000

    added buttons to toggle states

---
+++ buttons.js
@@ -0,0 +1,17 @@
+function normalDraw () {
+    console.log("button pressed");
+    var target = document.getElementById("hero_specialDraw");
+    target.classList.toggle("qa-hide");
+}
+
+function advancePlay () {
+    console.log("button pressed");
+    var target = document.getElementById("hero_specialDraw");
+    target.classList.toggle("advance");    
+
+}
+
+function specialDraw () {
+
+}
+
